ebbb55e10e99af6c68f0d9b9be6decae6e01b485
Markus Lewin <lewinmarkus@gmail.com>
2025-01-27 21:46:29 +0100

    Fetch data for home, budgets, and pots

--- src/app/budgets/page.tsx
+++ src/app/budgets/page.tsx
@@ -1,6 +1,48 @@
 import Link from "next/link";
+import { Fragment } from "react";
+import { now } from "~/app/_now";
+import { db } from "~/server/db";
 
-const BudgetsPage = () => {
+const BudgetsPage = async () => {
+  // Find all of this month's transaction for each budget. Pad with earlier transactions until we've found 3.
+  // todo: Types
+  // todo: Filter future transactions
+  const month = now.getUTCMonth() + 1;
+  const test = await db.$queryRaw`
+    SELECT
+      "Budget".id,
+      "Budget".maximum,
+      "Category".name AS "categoryName",
+      "Theme".color AS "themeColor",
+      t.id AS "transactionId",
+      t.amount AS "transactionAmount",
+      t.avatar AS "transactionAvatar",
+      t.date AS "transactionDate",
+      t.name AS "transactionName"
+    FROM
+      "Budget"
+        INNER JOIN "Category"
+          ON "Budget"."categoryId" = "Category".id
+        INNER JOIN "Theme"
+          ON "Budget"."themeId" = "Theme".id
+        INNER JOIN (
+          SELECT
+            *,
+            ROW_NUMBER () OVER (
+              PARTITION BY "categoryId"
+              ORDER BY date DESC
+            )
+          FROM "Transaction"
+        ) t
+          ON "Budget"."categoryId" = t."categoryId"
+    WHERE
+      DATE_PART('month', t.date) = ${month}
+        OR t.row_number <= 3
+    ORDER BY
+      "Budget"."createdAt",
+      date DESC
+    `;
+
   return (
     <>
       <h1 className="text-preset-1">Budgets</h1>
@@ -9,6 +51,18 @@ const BudgetsPage = () => {
           <span aria-hidden="true">+ </span>Add New Budget
         </Link>
       </p>
+      {Object.entries(Object.groupBy(test, (t) => t.id)).map(([id, rows]) => {
+        return (
+          <Fragment key={id}>
+            <h2 className="text-preset-2">{rows[0].categoryName}</h2>
+            <div
+              className="size-500"
+              style={{ background: rows[0].themeColor }}
+            />
+            <pre>{JSON.stringify(rows, undefined, "\t")}</pre>
+          </Fragment>
+        );
+      })}
       <p>
         <Link href={"/budgets/edit"}>Edit Budget</Link>
       </p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,73 @@
-const OverviewPage = () => {
+import { db } from "~/server/db";
+
+const OverviewPage = async () => {
+  const [balance, pots, transactions, budgets, recurringBills] =
+    await Promise.all([
+      db.balance.findFirst({
+        select: {
+          current: true,
+          expenses: true,
+          income: true,
+        },
+      }),
+      db.pot.findMany({
+        select: {
+          id: true,
+          name: true,
+          theme: {
+            select: {
+              color: true,
+            },
+          },
+          total: true,
+        },
+      }),
+      db.transaction.findMany({
+        take: 5,
+        select: {
+          id: true,
+          amount: true,
+          avatar: true,
+          date: true,
+          name: true,
+        },
+      }),
+      db.budget.findMany({
+        select: {
+          id: true,
+          theme: {
+            select: {
+              color: true,
+            },
+          },
+          category: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      }),
+      db.recurringBill.findMany({
+        select: {
+          amount: true,
+          day: true,
+        },
+      }),
+    ]);
+
   return (
     <>
       <h1 className="text-preset-1">Overview</h1>
+      <h2 className="text-preset-2">Balance</h2>
+      <pre>{JSON.stringify(balance, undefined, "\t")}</pre>
+      <h2 className="text-preset-2">Pots</h2>
+      <pre>{JSON.stringify(pots, undefined, "\t")}</pre>
+      <h2 className="text-preset-2">Budgets</h2>
+      <pre>{JSON.stringify(budgets, undefined, "\t")}</pre>
+      <h2 className="text-preset-2">Transactions</h2>
+      <pre>{JSON.stringify(transactions, undefined, "\t")}</pre>
+      <h2 className="text-preset-2">Recurring Bills</h2>
+      <pre>{JSON.stringify(recurringBills, undefined, "\t")}</pre>
     </>
   );
 };
