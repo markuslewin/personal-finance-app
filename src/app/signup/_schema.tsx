e2a31bcf5babbd9dad83418277688a044e453615
Markus Lewin <lewinmarkus@gmail.com>
2025-02-04 15:45:50 +0100

    Limit password length

--- src/app/signup/_schema.tsx
+++ src/app/signup/_schema.tsx
@@ -3,7 +3,13 @@ import { z } from "zod";
 export const schema = z.object({
   name: z.string(),
   email: z.string().email(),
-  "create-password": z.string(),
+  "create-password": z
+    .string()
+    // `bcrypt` uses only the first 72 bytes
+    .refine(
+      (val) => new TextEncoder().encode(val).length <= 72,
+      "Password is too long",
+    ),
 });
 
 export type Schema = z.infer<typeof schema>;
