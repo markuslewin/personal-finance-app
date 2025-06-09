124bbc27bc4793079a06db90e7e3653c64205fd4
Markus Lewin <lewinmarkus@gmail.com>
2025-06-09 16:53:39 +0200

    Migrate to mssql

--- README.md
+++ README.md
@@ -4,19 +4,38 @@
 
 ## Starting the project
 
-This project uses Docker for local development.
+The web app fetches data from an MS SQL database. The project contains configurations for managing a local database using Docker.
 
-The app service listens for file updates via a [bind mount](https://docs.docker.com/engine/storage/bind-mounts/). This seems to only work on Windows when using Docker with WSL 2.
+Data is persisted to the volume `mssql_data`.
 
-Data is persisted to the volume `postgres_data`.
+The connection string inside of `.env.example` points to the database server created by Docker Compose.
 
-- `docker compose -f compose.dev.yaml up`: Start Next and DB
-- `docker compose -f compose.dev.yaml --profile seed up`: Start Next and DB, and seed DB
-- `docker compose -f compose.dev.yaml down`: Stop services
+1. Start the app and database: `docker compose -f compose.dev.yaml up --build --watch`
+2. Apply database migrations: `npm run db:migrate`
+3. Seed with demo data (Optional): `npm run db:seed`
+
+Use `docker compose -f compose.dev.yaml down` to stop containers.
+
+---
+
+The common flow for schema changes:
+
+1. Test things out: `npm run db:push`
+2. Generate new migration: `npm run db:generate`
+3. Commit migration to version control
+
+Use `npm run db:reset` to reset the database.
+
+---
+
+Useful commands for managing volumes:
+
 - `docker volume rm <VOLUME NAME>`: Clear database by removing volume
 - `docker volume ls`: List volumes
 
-The production build can similarly be run locally, but with `-f compose.prod.yaml`.
+---
+
+The production build can be run locally with `docker compose -f compose.prod.yaml`.
 
 ## Welcome! 👋
 
---
+++ prisma/migrations/20250520162637_init/migration.sql
@@ -0,0 +1,141 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateTable
+CREATE TABLE [dbo].[Balance] (
+    [id] NVARCHAR(1000) NOT NULL,
+    [current] INT NOT NULL,
+    [income] INT NOT NULL,
+    [expenses] INT NOT NULL,
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [Balance_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updatedAt] DATETIME2 NOT NULL,
+    CONSTRAINT [Balance_pkey] PRIMARY KEY CLUSTERED ([id])
+);
+
+-- CreateTable
+CREATE TABLE [dbo].[Transaction] (
+    [id] NVARCHAR(1000) NOT NULL,
+    [avatar] NVARCHAR(1000) NOT NULL,
+    [name] NVARCHAR(1000) NOT NULL,
+    [date] DATETIME2 NOT NULL,
+    [amount] INT NOT NULL,
+    [categoryId] NVARCHAR(1000) NOT NULL,
+    [recurringBillId] NVARCHAR(1000),
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [Transaction_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updatedAt] DATETIME2 NOT NULL,
+    CONSTRAINT [Transaction_pkey] PRIMARY KEY CLUSTERED ([id])
+);
+
+-- CreateTable
+CREATE TABLE [dbo].[RecurringBill] (
+    [id] NVARCHAR(1000) NOT NULL,
+    [avatar] NVARCHAR(1000) NOT NULL,
+    [name] NVARCHAR(1000) NOT NULL,
+    [amount] INT NOT NULL,
+    [day] INT NOT NULL,
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [RecurringBill_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updatedAt] DATETIME2 NOT NULL,
+    CONSTRAINT [RecurringBill_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [RecurringBill_name_key] UNIQUE NONCLUSTERED ([name])
+);
+
+-- CreateTable
+CREATE TABLE [dbo].[Budget] (
+    [id] NVARCHAR(1000) NOT NULL,
+    [maximum] INT NOT NULL,
+    [categoryId] NVARCHAR(1000) NOT NULL,
+    [themeId] NVARCHAR(1000) NOT NULL,
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [Budget_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updatedAt] DATETIME2 NOT NULL,
+    CONSTRAINT [Budget_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [Budget_categoryId_key] UNIQUE NONCLUSTERED ([categoryId]),
+    CONSTRAINT [Budget_themeId_key] UNIQUE NONCLUSTERED ([themeId])
+);
+
+-- CreateTable
+CREATE TABLE [dbo].[Pot] (
+    [id] NVARCHAR(1000) NOT NULL,
+    [name] NVARCHAR(1000) NOT NULL,
+    [target] INT NOT NULL,
+    [total] INT NOT NULL,
+    [themeId] NVARCHAR(1000) NOT NULL,
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [Pot_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updatedAt] DATETIME2 NOT NULL,
+    CONSTRAINT [Pot_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [Pot_themeId_key] UNIQUE NONCLUSTERED ([themeId])
+);
+
+-- CreateTable
+CREATE TABLE [dbo].[Category] (
+    [id] NVARCHAR(1000) NOT NULL,
+    [name] NVARCHAR(1000) NOT NULL,
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [Category_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updatedAt] DATETIME2 NOT NULL,
+    CONSTRAINT [Category_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [Category_name_key] UNIQUE NONCLUSTERED ([name])
+);
+
+-- CreateTable
+CREATE TABLE [dbo].[Theme] (
+    [id] NVARCHAR(1000) NOT NULL,
+    [name] NVARCHAR(1000) NOT NULL,
+    [color] NVARCHAR(1000) NOT NULL,
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [Theme_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updatedAt] DATETIME2 NOT NULL,
+    CONSTRAINT [Theme_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [Theme_name_key] UNIQUE NONCLUSTERED ([name])
+);
+
+-- CreateTable
+CREATE TABLE [dbo].[User] (
+    [id] NVARCHAR(1000) NOT NULL,
+    [email] NVARCHAR(1000) NOT NULL,
+    [name] NVARCHAR(1000) NOT NULL,
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [User_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updatedAt] DATETIME2 NOT NULL,
+    CONSTRAINT [User_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [User_email_key] UNIQUE NONCLUSTERED ([email])
+);
+
+-- CreateTable
+CREATE TABLE [dbo].[Password] (
+    [id] NVARCHAR(1000) NOT NULL,
+    [hash] NVARCHAR(1000) NOT NULL,
+    [userId] NVARCHAR(1000) NOT NULL,
+    [createdAt] DATETIME2 NOT NULL CONSTRAINT [Password_createdAt_df] DEFAULT CURRENT_TIMESTAMP,
+    [updatedAt] DATETIME2 NOT NULL,
+    CONSTRAINT [Password_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [Password_userId_key] UNIQUE NONCLUSTERED ([userId])
+);
+
+-- AddForeignKey
+ALTER TABLE [dbo].[Transaction] ADD CONSTRAINT [Transaction_categoryId_fkey] FOREIGN KEY ([categoryId]) REFERENCES [dbo].[Category]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE [dbo].[Transaction] ADD CONSTRAINT [Transaction_recurringBillId_fkey] FOREIGN KEY ([recurringBillId]) REFERENCES [dbo].[RecurringBill]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE [dbo].[Budget] ADD CONSTRAINT [Budget_categoryId_fkey] FOREIGN KEY ([categoryId]) REFERENCES [dbo].[Category]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE [dbo].[Budget] ADD CONSTRAINT [Budget_themeId_fkey] FOREIGN KEY ([themeId]) REFERENCES [dbo].[Theme]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE [dbo].[Pot] ADD CONSTRAINT [Pot_themeId_fkey] FOREIGN KEY ([themeId]) REFERENCES [dbo].[Theme]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE [dbo].[Password] ADD CONSTRAINT [Password_userId_fkey] FOREIGN KEY ([userId]) REFERENCES [dbo].[User]([id]) ON DELETE CASCADE ON UPDATE CASCADE;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
