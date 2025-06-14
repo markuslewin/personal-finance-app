7c5c2517dac7f2449d83bc3e116a9bbce812d0e8
Markus Lewin <lewinmarkus@gmail.com>
2025-06-14 17:25:04 +0200

    Fix chunk load error for parallel routes

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// Next expects encoded paths, but Azure App Service decodes some characters
+// Incorrect: `/_next/static/chunks/app/@modal/(.)photos/%5Bid%5D/page-e4ba477351e5f10d.js`
+// Correct: `/_next/static/chunks/app/%40modal/(.)photos/%5Bid%5D/page-e4ba477351e5f10d.js`
+export const middleware = (request: NextRequest) => {
+  return NextResponse.rewrite(request.url.replace("@", "%40"));
+};
+
+// export const config = {
+//   matcher: "/about/:path*",
+// };
