f3823b3f5c364f9539ffabab855ec9f45ddbe9b9
Markus Lewin <lewinmarkus@gmail.com>
2026-01-11 12:04:43 +0100

    Codemod to next 16

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 // Next expects encoded paths, but Azure App Service decodes some characters
 // Incorrect: `/_next/static/chunks/app/@modal/(.)photos/%5Bid%5D/page-e4ba477351e5f10d.js`
 // Correct: `/_next/static/chunks/app/%40modal/(.)photos/%5Bid%5D/page-e4ba477351e5f10d.js`
-export const middleware = (request: NextRequest) => {
+export const proxy = (request: NextRequest) => {
   return NextResponse.rewrite(request.url.replace("@", "%40"));
 };
 
